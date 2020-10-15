fa26f3a564d54b8b71412e3dc98b5e78224f1700
Wyatt Senalik <55900622+Lvl1Lasagna@users.noreply.github.com>
2020-10-15 18:54:23 -0500

    Got some work some work on the gun
    Can swap to gun view by right clicking. Left clicking fires the gun.
    Currently has infinite bullets, this needs to be changed.
    Also camera swapping is weird sometimes, this needs to be fixed.

---
+++ Katana Cowboy/Assets/Scripts/Gun/MoveObjectInfinitly.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectInfinitly : MonoBehaviour
+{
+    // The speed the object should move.
+    [SerializeField]
+    private float speed = 1f;
+    // The direction the object should move locally.
+    [SerializeField]
+    private Vector3 localMoveDirection = Vector3.up; 
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(MoveCoroutine());   
+    }
+
+    /// <summary>
+    /// Moves the object in the specified direction.
+    /// </summary>
+    /// <returns>IEnumerator</returns>
+    private IEnumerator MoveCoroutine()
+    {
+        // Continuously update the position of the object.
+        while (true)
+        {
+            this.transform.position += this.transform.rotation * localMoveDirection * speed * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Sets the local direction to move the object in.
+    /// </summary>
+    /// <param name="_moveDir_">Direction to move the object in. Should be normalized.</param>
+    public void SetMoveDirection(Vector3 _moveDir_)
+    {
+        localMoveDirection = _moveDir_;
+    }
+}
---
+++ Katana Cowboy/Assets/Scripts/Gun/PushShootable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushShootable : Shootable
+{
+    // Customizable.
+    // Magnitude of force to apply.
+    [SerializeField]
+    private float forceMag = 30f;
+
+    // References.
+    // Reference to the attached rigidbody.
+    private Rigidbody rb;
+
+    // Called 0th.
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Override form Shootable.
+    /// Applies a force to the object and resets health.
+    /// </summary>
+    protected override void Die()
+    {
+        Vector3 force = shotInfo.fromDir * forceMag;
+        rb.AddForceAtPosition(force, shotInfo.point);
+        curHealth = maxHealth;
+    }
+}
