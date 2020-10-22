61b586debe6aa58be8a28eb323c25beaa1247784
Wyatt Senalik <55900622+Lvl1Lasagna@users.noreply.github.com>
2020-10-22 18:44:37 -0500

    Manual merge from Wyatt branch

---
+++ Katana Cowboy/Assets/Scripts/Gun/DestroyAfterDelay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    // The amount of seconds to wait before the object is destroyed.
+    [SerializeField]
+    private float secondsToWait = 1f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Start the death coroutine.
+        StartCoroutine(DeathCoroutine());
+    }
+
+    /// <summary>
+    /// Coroutine.
+    /// Waits the specified amount of seconds and then destroys this object.
+    /// </summary>
+    /// <returns>IEnumerator</returns>
+    private IEnumerator DeathCoroutine()
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        Destroy(this.gameObject);
+        yield return null;
+    }
+}
---
+++ Katana Cowboy/Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public abstract class Pickup : MonoBehaviour
+{
+    // Name of the layer that can be shot.
+    public static readonly string PICKUP_LAYER_NAME = "Pickup";
+
+    // Called before the first frame.
+    private void Start()
+    {
+        // If we are changing the layer from some other specified layer, let me know.
+        if (this.gameObject.layer != LayerMask.NameToLayer("Default") &&
+            this.gameObject.layer != LayerMask.NameToLayer(PICKUP_LAYER_NAME))
+        {
+            Debug.Log("Changed " + this.gameObject.name + "'s layer from " +
+               LayerMask.LayerToName(this.gameObject.layer) + " to " + PICKUP_LAYER_NAME);
+        }
+        // Set the layer for this gameobject.
+        this.gameObject.layer = LayerMask.NameToLayer(PICKUP_LAYER_NAME);
+    }
+
+    /// <summary>
+    /// Called when the pickup is picked up.
+    /// <param name="_picker_">Picker script that picked this pickup up.</param>
+    /// </summary>
+    public abstract void Pick(Picker _picker_);
+}
