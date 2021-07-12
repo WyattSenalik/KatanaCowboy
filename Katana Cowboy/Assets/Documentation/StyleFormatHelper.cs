59989d49d1272a64c361a5d8841c0e64f1704459
SenalikW0106 <wsenalik@hotmail.com>
2021-07-12 13:24:30 -0500

    StyleFormat Guide

---
+++ Katana Cowboy/Assets/Documentation/StyleFormatHelper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace OtherExampleNamespace
+{
+    /// <summary>
+    /// Helper for the style format example.
+    /// </summary>
+    public class StyleFormatHelper : MonoBehaviour
+    {
+        // Amount of monobehaviours attached to this script
+        public int AmountBehaviours => amountBehavs;
+        private int amountBehavs = 0;
+
+
+        // Domestic Initialization
+        private void Awake()
+        {
+            MonoBehaviour[] behavs = GetComponents<MonoBehaviour>();
+            amountBehavs = behavs.Length;
+        }
+    }
+}
