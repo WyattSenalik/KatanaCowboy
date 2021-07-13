3bd0962698d05f1054a0152051ea7cbb95ce11fb
SenalikW0106 <wsenalik@hotmail.com>
2021-07-13 16:53:12 -0500

    test

--- Katana Cowboy/Assets/Scripts/PlayerController.cs
+++ Katana Cowboy/Assets/Scripts/PlayerController.cs
@@ -50,7 +50,7 @@ public class PlayerController : MonoBehaviour
 
     private bool isSubscribed = false;
 
-    private string testVariable = EventIDList.TestEvent11;
+    private string testVariable = EventIDList.TestEvent13;
 
 
     // Functions called by unity messages (ex: Start, Awake, Update, etc.)
