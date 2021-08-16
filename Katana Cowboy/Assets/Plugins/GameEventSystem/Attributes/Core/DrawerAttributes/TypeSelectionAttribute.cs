4302e453dc1672bf228df6779705a289c4624636
SenalikW0106 <wsenalik@hotmail.com>
2021-08-16 13:49:57 -0500

    Tried to do custom drawer for Types. I'll probably delete this, but wanted it somewhere if I ever wanted to look again.

---
+++ Katana Cowboy/Assets/Plugins/GameEventSystem/Attributes/Core/DrawerAttributes/TypeSelectionAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameEventSystemAttributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class TypeSelectionAttribute : PropertyAttribute
+    {
+
+    }
+}
