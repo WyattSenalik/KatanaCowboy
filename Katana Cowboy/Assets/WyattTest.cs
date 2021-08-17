8b039ef81421d210a9b512ab635fae0000284953
SenalikW0106 <wsenalik@hotmail.com>
2021-08-17 00:49:56 -0500

    MyTest

---
+++ Katana Cowboy/Assets/WyattTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GameEventSystem;
+
+public class WyattTest : MonoBehaviour
+{
+    private void Test()
+    {
+        GameEventIdentifier<UnityEngine.InputSystem.InputAction.CallbackContext> testEvent = EventIDList.Test1;
+    }
+}
