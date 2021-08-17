ec4e3cd6bbbcf67b26eae1151e40b794bdaf055d
es-vang <29257863+enejii@users.noreply.github.com>
2021-08-17 00:48:58 -0500

    NewTestPush

---
+++ Katana Cowboy/Assets/NewTestScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GameEventSystem;
+
+public class NewTestScript : MonoBehaviour
+{
+  private void NewTest()
+  {
+    GameEventIdentifier<UnityEngine.InputSystem.InputAction.CallbackContext> testEvent = EventIDList.NewTest;
+  }
+}
