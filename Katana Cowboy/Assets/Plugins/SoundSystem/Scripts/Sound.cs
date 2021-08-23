511b52b2a86a4fdf28ade9bff384e70746aade8b
SenalikW0106 <wsenalik@hotmail.com>
2021-08-22 21:57:53 -0500

    Work on sound system

---
+++ Katana Cowboy/Assets/Plugins/SoundSystem/Scripts/Sound.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+using SoundSystem.Internal;
+
+namespace SoundSystem
+{
+    /// <summary>
+    /// Scriptable Object to hold sound data to be played by the SoundManager.
+    /// </summary>
+    [CreateAssetMenu(fileName = "new Sound", menuName = "ScriptableObjects/SoundSystem/Sound")]
+    public class Sound : ScriptableObject, ISound
+    {
+        public AudioClip GetClip() => clip;
+        [SerializeField] private AudioClip clip = null;
+
+        public AudioMixerGroup GetMixerGroup() => mixer;
+        [SerializeField] private AudioMixerGroup mixer = null;
+
+        public bool IsLoop() => isLoop;
+        [SerializeField] private bool isLoop = false;
+
+        public int GetPriority() => priority;
+        [SerializeField, Range(0, 256)] private int priority = 128;
+
+        public float GetVolume() => volume;
+        [SerializeField, Range(0.0f, 1.0f)] private float volume = 1.0f;
+
+        public float GetPitch() => pitch;
+        [SerializeField, Range(-3.0f, 3.0f)] private float pitch = 1.0f;
+
+        public float GetStereoPan() => stereoPan;
+        [SerializeField, Range(-1.0f, 1.0f)] private float stereoPan = 0.0f;
+
+        public float GetSpatialBlend() => spatialBlend;
+        [SerializeField, Range(0.0f, 1.0f)] private float spatialBlend = 0.0f;
+
+        public float GetReverbZoneMix() => reverbZoneMix;
+        [SerializeField, Range(0.0f, 1.1f)] private float reverbZoneMix = 1.0f;
+
+        public bool IsScenePersistent() => isScenePersistent;
+        [SerializeField] private bool isScenePersistent = false;
+
+
+        // Sound
+        public bool Play()
+        {
+            return SoundManager.Play(this);
+        }
+        public bool Pause()
+        {
+            return SoundManager.Pause(this);
+        }
+        public bool Load()
+        {
+            return SoundManager.Load(this);
+        }
+        public bool Release()
+        {
+            return SoundManager.Release(this);
+        }
+        public bool Unload()
+        {
+            return SoundManager.Unload(this);
+        }
+        // Music
+        public bool PlayMusic()
+        {
+            return SoundManager.PlayMusic(this);
+        }
+        public bool PauseMusic()
+        {
+            return SoundManager.PauseMusic(this);
+        }
+        public bool LoadMusic()
+        {
+            return SoundManager.LoadMusic(this);
+        }
+        public bool ReleaseMusic()
+        {
+            return SoundManager.ReleaseMusic(this);
+        }
+        public bool UnloadMusic()
+        {
+            return SoundManager.UnloadMusic(this);
+        }
+    }
+}
---
+++ Katana Cowboy/Assets/Plugins/SoundSystem/Scripts/SoundManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SoundSystem.Internal;
+
+namespace SoundSystem
+{
+    public static class SoundManager
+    {
+        private static ISound2DPool persistentFlatSoundPool = new Sound2DPool("Persistent 2DSound Pool", true);
+        private static ISound2DPool musicPool = new Sound2DPool("Music Pool", true);
+        private static ISound2DPool flatSoundPool = new Sound2DPool("2DSound Pool", false);
+
+
+        public static bool Play(ISound sound)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool Pause(ISound sound)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool Load(ISound sound)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool Release(ISound sound)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool Unload(ISound sound)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool PlayMusic(ISound music)
+        {
+            return musicPool.Play(music);
+        }
+        public static bool PauseMusic(ISound music)
+        {
+            return musicPool.Pause(music);
+        }
+        public static bool LoadMusic(ISound music)
+        {
+            return musicPool.Add(music);
+        }
+        public static bool ReleaseMusic(ISound music)
+        {
+            return musicPool.Release(music);
+        }
+        public static bool UnloadMusic(ISound music)
+        {
+            return musicPool.Remove(music);
+        }
+    }
+}
